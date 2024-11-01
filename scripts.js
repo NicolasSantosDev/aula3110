16d5dc6048e1d79ce31c3a5ce1b4041ca7805956
NICOLAS DOS SANTOS SANTANA <nicolas.santana20@etec.sp.gov.br>
2024-10-31 21:32:07 -0300

    App aula3110 v01

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function botao1() {
+    alert ('Você clicou no Botão 1')
+}
+
+function botao2() {
+    alert ('Você clicou no Botão 2')
+}
+
+function botao3() {
+    alert ('Você clicou no Botão 3')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
